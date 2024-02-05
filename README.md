3ec8c118084f915051be12803ccb0dc51c5e8fe0
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-02-05 12:55:04 -0500

    First test case : testing root path

---
+++ README.md
@@ -0,0 +1,7 @@
+### GAMEVIZE
+This is a project to learn Javascript, Node and Express. Right now it supports logging in to steam account. 
+
+Next : Integrate Steam API to get game stats 
+
+To run the app `npm start`
+To run tests `npm run test`
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,1 +1,12 @@
-const request = re;
+const request = require("supertest");
+const app = require("../src/app");
+describe("Test the root path", () => {
+  test("It should respond to GET method", (done) => {
+    request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
