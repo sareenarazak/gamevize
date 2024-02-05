f59a2237a141a851b44de2f95b852f5ad2a2b421
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-02-05 11:30:15 -0500

    Formatting using eslint and prettier

--- app.js
+++ app.js
@@ -1,92 +1,95 @@
-const express = require('express');
-const passport = require('passport');
-const authRoutes = require('./routes/auth');
+const express = import("express");
+const passport = require("passport");
+const authRoutes = require("./routes/auth");
 
-const session = require('express-session');
-const SteamStrategy = require('passport-steam').Strategy;
-const logger = require('morgan');
+const session = require("express-session");
+const SteamStrategy = require("passport-steam").Strategy;
+const logger = require("morgan");
 
-const path = require('path');
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config();
 
 const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/');
+  res.redirect("/");
 };
 
 // Passport session set up for persistent login sessions
 // serialize and deserialize user info
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
 // Stream login strategy set up
-passport.use(new SteamStrategy({
-  returnURL: 'http://localhost:3000/auth/steam/return',
-  realm: 'http://localhost:3000/',
-  apiKey: process.env.STEAM_API_KEY,
-},
-function(identifier, profile, done) {
-  process.nextTick(function() {
-    profile.identifier = identifier;
-    return done(null, profile);
-  });
-},
-));
+passport.use(
+  new SteamStrategy(
+    {
+      returnURL: "http://localhost:3000/auth/steam/return",
+      realm: "http://localhost:3000/",
+      apiKey: process.env.STEAM_API_KEY,
+    },
+    function (identifier, profile, done) {
+      process.nextTick(function () {
+        profile.identifier = identifier;
+        return done(null, profile);
+      });
+    },
+  ),
+);
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // session set up
-app.use(session({
-  secret: 'your secret',
-  name: 'name of session id',
-  resave: true,
-  saveUninitialized: true,
-}));
-
+app.use(
+  session({
+    secret: "your secret",
+    name: "name of session id",
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-  res.render('index', {user: req.user});
+app.get("/", function (req, res) {
+  res.render("index", { user: req.user });
 });
 
-app.get('/account', ensureAuthenticated, function(req, res) {
-  res.render('account', {user: req.user});
+app.get("/account", ensureAuthenticated, function (req, res) {
+  res.render("account", { user: req.user });
 });
 
-app.post('/logout', function(req, res, next) {
+app.post("/logout", function (req, res, next) {
   req.logout((err) => {
     if (err) {
       return next(err);
     }
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
 // See views/auth.js for authentication routes
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-},
-);
+});
 
 module.exports = app;
