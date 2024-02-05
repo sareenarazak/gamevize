1124ddbf30dd6c6dc0befb514a7fc7f1cd5b0808
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-02-05 11:54:43 -0500

    Revert "Formatting using eslint and prettier"
    This reverts commit f59a2237a141a851b44de2f95b852f5ad2a2b421.

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
+const passport = require('passport');
 
 // GET /auth/steam handling --> Steam redirects user to /auth/steam/return
-router.get(
-  "/steam",
-  passport.authenticate("steam", { failureRedirect: "/" }),
-  function (req, res) {
-    res.redirect("/");
-  },
-);
+router.get('/steam',
+    passport.authenticate('steam', {failureRedirect: '/'}),
+    function(req, res) {
+      res.redirect('/');
+    });
 
-router.get(
-  "/steam/return",
-  passport.authenticate("steam", { failureRedirect: "/" }),
-  function (req, res) {
-    res.redirect("/");
-  },
-);
+router.get('/steam/return',
+    passport.authenticate('steam', {failureRedirect: '/'}),
+    function(req, res) {
+      res.redirect('/');
+    });
 
 module.exports = router;
